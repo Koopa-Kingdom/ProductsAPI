f390d459a2a4962e56650ed419cded32334b09b4
gthellter <drewsworld16@gmail.com>
2022-11-08 13:12:41 -0600

    added loderIO res

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get('/loaderio-09ae7bd3da1ef357bf0b33adf0941b75', (req, res) => {
+  res.send('loaderio-09ae7bd3da1ef357bf0b33adf0941b75');
+});
 
 app.use('/api/fec2/hr-rfc/products', routes);
 
